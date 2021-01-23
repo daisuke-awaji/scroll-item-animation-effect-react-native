dbbd68be8ad7f6dfd1efe56b776e47be619c8469
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2021-01-23 15:12:13 +0900

    scroll-item-animation-effect

--- App.tsx
+++ App.tsx
@@ -1,21 +1,131 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  StatusBar,
+  Animated,
+} from "react-native";
+import faker from "faker";
+
+faker.seed(10);
+
+// type DataType = Partial<{
+//   key: string;
+//   image: string;
+//   name: string;
+//   jobTitle: string;
+//   email: string;
+// }>;
+
+const DATA = [...Array(30).keys()].map((_, i) => {
+  return {
+    key: faker.random.uuid(),
+    image: `https://randomuser.me/api/portraits/${faker.helpers.randomize([
+      "women",
+      "men",
+    ])}/${faker.random.number(60)}.jpg`,
+    name: faker.name.findName(),
+    jobTitle: faker.name.jobTitle(),
+    email: faker.internet.email(),
+  };
+});
+
+const BACKGROUND_IMAGE =
+  "https://images.unsplash.com/photo-1568120294838-abf43a5f4bd6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80";
+
+const SPACING = 20;
+const AVATER_SIZE = 70;
+const ITEM_SIZE = AVATER_SIZE + SPACING * 3;
 
 export default function App() {
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Image
+        source={{ uri: BACKGROUND_IMAGE }}
+        style={StyleSheet.absoluteFillObject}
+        blurRadius={5}
+      />
+      <Animated.FlatList
+        data={DATA}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: true }
+        )}
+        keyExtractor={(item) => item.key}
+        contentContainerStyle={{
+          padding: SPACING,
+          paddingTop: StatusBar.currentHeight || 42,
+        }}
+        renderItem={({ item, index }) => {
+          const inputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + 2),
+          ];
+          const opacityInputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + 2),
+          ];
+
+          const scale = scrollY.interpolate({
+            inputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+          const opacity = scrollY.interpolate({
+            inputRange: opacityInputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+          return (
+            <Animated.View
+              key={index}
+              style={{
+                flexDirection: "row",
+                padding: SPACING,
+                marginVertical: SPACING / 2,
+                backgroundColor: "rgba(255, 255,255,0.8)",
+                borderRadius: 12,
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 10,
+                },
+                shadowOpacity: 0.3,
+                shadowRadius: 20,
+                opacity,
+                transform: [{ scale }],
+              }}
+            >
+              <Image
+                source={{ uri: item.image }}
+                style={{
+                  width: AVATER_SIZE,
+                  height: AVATER_SIZE,
+                  borderRadius: AVATER_SIZE,
+                  marginEnd: SPACING / 2,
+                }}
+              />
+              <View>
+                <Text style={{ fontSize: 22, fontWeight: "700" }}>
+                  {item.name}
+                </Text>
+                <Text style={{ fontSize: 18, opacity: 0.7 }}>
+                  {item.jobTitle}
+                </Text>
+                <Text style={{ fontSize: 14, opacity: 0.8, color: "#0099cc" }}>
+                  {item.email}
+                </Text>
+              </View>
+            </Animated.View>
+          );
+        }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
